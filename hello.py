713f4278b03ca9b214bd0b394e259e6a2808e32c
luo.reid <luo.reid@gmail.com>
2021-12-29 21:15:51 -0800

    feat: event loop for python

--- hello.py
+++ hello.py
@@ -1,17 +1,28 @@
 from threading import Thread
-from time import sleep,time
+import selectors
+import sys
+from time import sleep, time
 from typing import Tuple
 from fib import timed_fib
 
+
+def process_input(stream):
+    text = stream.readline()
+    n = int(text.strip())
+    print('fib({}) = {}'.format(n, timed_fib(n)))
+
+
 def print_hello():
     while True:
-        print("{} - Hello world!".format(int(time())))
+        print("{} - Hello world:)".format(int(time())))
         sleep(3)
+
 
 def read_and_process_input():
     while True:
         n = int(input())
-        print('fib({}) = {}'.format(n,timed_fib(n)))
+        print('fib({}) = {}'.format(n, timed_fib(n)))
+
 
 def main():
     t = Thread(target=print_hello)
@@ -19,5 +30,19 @@ def main():
     t.start()
     read_and_process_input()
 
+
+def main1():
+    selector = selectors.DefaultSelector()
+    selector.register(sys.stdin, selectors.EVENT_READ)
+    last_hello = 0
+    while True:
+        for [event, mask,t] in selector.select(0.1):
+            print(event,mask,t)
+            process_input(event.fileobj)
+        if time() - last_hello > 3:
+            last_hello = time()
+            print_hello()
+
+
 if __name__ == '__main__':
-    main()
+    main1()
