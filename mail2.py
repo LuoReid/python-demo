cd77a70928c8ecf55e4e9c79655c9d0158961bbc
= <=>
2022-04-20 19:58:42 +0800

    feat: mail demo

---
+++ mail2.py
@@ -0,0 +1,29 @@
+from email.utils import parseaddr, formataddr
+from email.header import Header
+from email import encoders
+import smtplib
+from email.mime.text import MIMEText
+
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr((Header(name, 'utf-8').encode(), addr))
+
+
+from_addr = input('From:')
+password = input('Password:')
+to_addr = input('To:')
+smtp_server = input('SMTP Server:')
+
+
+msg = MIMEText('hello,send you by Python...', 'plain', 'utf-8')
+msg['From'] = _format_addr('Python爱好者 <%s>' % from_addr)
+msg['To'] = _format_addr('管理员 <%s>' % to_addr)
+msg['Subject'] = Header('from SMTP say hello...', 'utf-8').encode()
+
+server = smtplib.SMTP(smtp_server, 25)
+
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
