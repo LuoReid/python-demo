ce11edcc869cbc7ae6e81018fe423345a37bcbbb
= <=>
2022-04-28 21:08:32 +0800

    feat: config

--- web/www/config.py
+++ web/www/config.py
@@ -20,30 +20,33 @@ class Dict(dict):
     def __setattr__(self, key, value):
         self[key] = value
 
-    def merge(defaults, override):
-        r = {}
-        for k, v in defaults.items():
-            if k in override:
-                if isinstance(v, dict):
-                    r[k] = merge(v, override[k])
-                else:
-                    r[k] = override[k]
+
+def merge(defaults, override):
+    r = {}
+    for k, v in defaults.items():
+        if k in override:
+            if isinstance(v, dict):
+                r[k] = merge(v, override[k])
             else:
-                r[k] = v
-        return r
+                r[k] = override[k]
+        else:
+            r[k] = v
+    return r
 
-    def toDict(d):
-        D = Dict()
-        for k, v in d.items():
-            D[k] = toDict(v) if isinstance(v, dict) else v
-        return D
 
-    configs = config_default.configs
+def toDict(d):
+    D = Dict()
+    for k, v in d.items():
+        D[k] = toDict(v) if isinstance(v, dict) else v
+    return D
 
-    try:
-        import config_override
-        configs = merge(configs, config_override.configs)
-    except ImportError:
-        pass
 
-    configs = toDict(configs)
+configs = config_default.configs
+
+try:
+    import config_override
+    configs = merge(configs, config_override.configs)
+except ImportError:
+    pass
+
+configs = toDict(configs)
--- web/www/config_default.py
+++ web/www/config_default.py
@@ -1,5 +1,5 @@
 configs = {
-
+    'debug': True,
     'db': {
         'host': '127.0.0.1', 'port': 3306,
         'user': 'www-data', 'password': 'www-data', 'database': 'awesome'
