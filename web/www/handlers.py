bb60bcd65be4b21589890818b3d26d3f7bd01f92
= <=>
2022-04-29 22:00:50 +0800

    feat: api

--- web/www/handlers.py
+++ web/www/handlers.py
@@ -28,3 +28,17 @@ async def index(request):
              created_at=time.time()-7200)
     ]
     return {'__template__': 'blogs.html', 'blogs': blogs}
+
+
+@get('/api/users')
+def api_get_users(*, page='1'):
+    # page_index = get_page_index(page)
+    # num = yield from User.findNumber('count(id)')
+    # p = Page(num,page_index)
+    # if num == 0:
+    #     return dict(page=p,users=())
+    # users = yield from User.findAll(orderBy='created_at desc',limit=(p.offset,p.limit))
+    us = yield from User.findAll(orderBy='created_at desc')
+    for u in us:
+        u.passwd = '******'
+    return dict(users=us)
